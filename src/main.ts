e8121b29fceab06d19f96199cca24ee7cebf552c
Tomáš Šromovský <tomas.sromovsky@posam.sk>
2025-12-01 19:07:05 +0100

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3000;
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.enableCors();
     app.useGlobalPipes(new ValidationPipe({
         whitelist: true,
         forbidNonWhitelisted: true,
